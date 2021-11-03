5ffb0cc90fb1249a656997dda4811ce68ecf349c
schafhierte204 <hafejens@gmail.com>
2021-11-03 14:09:51 +0100

    Hauptklasse erstellt

---
+++ src/Hangman.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Lenovo
+ *
+ */
+public class Hangman {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
