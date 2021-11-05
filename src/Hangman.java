9b70b5ea657809278dbef127865c313b284e9670
schafhierte204 <hafejens@gmail.com>
2021-11-05 09:04:18 +0100

    funktion zum sting füllen hinzugefügt

--- src/Hangman.java
+++ src/Hangman.java
@@ -19,7 +19,17 @@ public class Hangman {
 	 * @return String :neues wort zur Ausgabe
 	 */
 	static String ausgabe(String wort, char eingabe) {
+
 		return wort;
+	}
+
+	static String fuellen(String zuratewort, char buchstabe ) {
+		int lange = zuratewort.length();
+		StringBuilder ausgabe = new StringBuilder(lange);
+		for (int i = 0; i < lange; i++) {
+			ausgabe.append(buchstabe);
+		}
+		return ausgabe.toString();
 	}
 
 	static boolean istEnthalten(String wort, char eingabe) {
