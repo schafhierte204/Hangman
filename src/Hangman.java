3a87b155beaed7698509471c9b980f067613f2f1
vollmani <vollmani@student.lbk.lippe.de>
2021-11-08 10:11:15 +0100

    schleife

--- src/Hangman.java
+++ src/Hangman.java
@@ -15,7 +15,9 @@ public class Hangman {
 	 */
 	public static void main(String[] args) {
 		
+		String wort = "Haus";
 		Scanner in = new Scanner(System.in);
+		int versuche =15;
 		
 		
 		System.out.println("HANGMAN");
@@ -24,11 +26,13 @@ public class Hangman {
 		System.out.println("Sie haben 15 Versuche.");
 		System.out.println("Geben Sie einen Buchstaben ein:");
 		char wahl = in.next().charAt(0);
-		
+		for(int i= 1;i<=versuche;i++)
+		{
 		System.out.println("ist falsch! Sie haben noch 14 Versuche");
 		System.out.println("");
 		System.out.println("**************************************");
 		System.out.println("");
+		}
 		
 		in.close();
 	
