9b7e850df0a5652914e3469494abfa9ba6839431
schafhierte204 <hafejens@gmail.com>
2021-11-08 09:59:40 +0100

    funktion zur ausgabe hinzugefügt

--- src/Hangman.java
+++ src/Hangman.java
@@ -11,19 +11,28 @@ public class Hangman {
 	/**
 	 * 
 	 * @param wort
-	 *                <p>
-	 *                :die zuletzt angezeigte Ausgabe<br>
-	 *                nicht erratenen buchstaben werden mit "_" Dargestellt
-	 *                </p>
-	 * @param eingabe :Der Eingegebene Buchstabe
-	 * @return String :neues wort zur Ausgabe
+	 *                 <p>
+	 *                 die zuletzt angezeigte Ausgabe<br>
+	 *                 nicht erratenen buchstaben werden mit "_" Dargestellt
+	 *                 </p>
+	 * @param ratewort Das zu erratene Wort
+	 * @param eingabe  Der Eingegebene Buchstabe
+	 * @return String neues wort zur Ausgabe
 	 */
-	static String ausgabe(String wort, char eingabe) {
+	static String ausgabe(String wort, String ratewort, char eingabe) {
+		int i = ratewort.indexOf(eingabe);
+		if (i == -1)
+			return wort;// buchstabe nicht enthalten
+		StringBuilder sbWort= new StringBuilder(wort);
+		while (i !=-1) {
+			sbWort.setCharAt(i, eingabe);
+			i= ratewort.indexOf(eingabe, i+1);
+		}
 
-		return wort;
+		return sbWort.toString();
 	}
 
-	static String fuellen(String zuratewort, char buchstabe ) {
+	static String fuellen(String zuratewort, char buchstabe) {
 		int lange = zuratewort.length();
 		StringBuilder ausgabe = new StringBuilder(lange);
 		for (int i = 0; i < lange; i++) {
